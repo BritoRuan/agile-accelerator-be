b900b52a0d58692354492cea1fdb000888c082c7
Ruan Brito <ruan.desenvolvedor@gmail.com>
2025-01-08 11:14:42 -0300

    feat: added products repository

---
+++ src/module/repositories/contracts/ProductsRepository.ts
@@ -0,0 +1,10 @@
+import { Prisma, Product } from '@prisma/client'
+
+export interface ProductsRepository {
+  create(data: Prisma.ProductCreateInput): Promise<Product>
+  findMany(): Promise<Product[]>
+  update(id: string, data: Prisma.ProductUpdateInput): Promise<Product>
+  delete(id: string): Promise<boolean>
+  findProductById(id: string): Promise<Product | null>
+  findProductByName(nome: string): Promise<Product[]>
+}
